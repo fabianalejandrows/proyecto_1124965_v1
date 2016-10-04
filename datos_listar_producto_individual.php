0f53ad67c2b7f8a61694bcdfae472bc5c5eb1971
FABIÁN ALEJANDRO RODRÍGUEZ LÓPEZ <fabianalejandro@misena.edu.co>
2016-10-04 07:20:39 -0500

    Update datos_listar_producto_individual.php
    Un nuevo cambio en el nombre

--- datos_listar_producto_individual.php
+++ datos_listar_producto_individual.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Inicio-Ferretería</title>
+<title>Inicio-Ferreteríaaa</title>
 </head>
 
 <body>
